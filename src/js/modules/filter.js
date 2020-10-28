706f7f7d587451cf7a9def80877384bca9f10138
Марина Бабенко <babenko.m@wezom.com.ua>
2020-10-28 19:58:53 +0200

    pagination

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -3,18 +3,24 @@
  */
 
 import $ from 'js#/lib/jquery';
+import goods from 'js#/data/goods.json';
 
 class Filter {
-	constructor(formSelector, paginationSelector) {
-		this.filter = $(formSelector).get(0);
+	constructor() {
+		this.filter = $('#filter').get(0);
 
 		if (!this.filter) {
 			return;
 		}
 
 		this.$filter = $(this.filter);
-		this.$pagination = $(paginationSelector);
-		this.elements = this.filter?.elements || [];
+		this.$pagination = $('#pagination');
+		this.elements = this.filter?.elements;
+		this.$cardTemplate = $(window.document.querySelector('#card-template')?.content);
+		this.$paginationTemplate = $(window.document.querySelector('#pagination-template')?.content);
+		this.$list = $('#card-list');
+		this.list = goods || [];
+		this.filterList = goods || [];
 		this.keys = [
 			{ form: 'brand', server: 'brand', param: 'brand', type: 'string' },
 			{
@@ -61,15 +67,24 @@ class Filter {
 			pagination: {
 				sort: this.elements.sort.value,
 				perPage: +this.elements.per_page.value,
-				page: +this.$pagination.find('.is-active').text()
+				page: 1
 			}
 		};
+		this.data = this.assign(this.initialData);
 
 		for (let i = 0; i < this.elements.length; i++) {
 			$(this.elements[i]).on('change', () => {
-				this.filtering();
+				this.render();
 			});
 		}
+		$(window.document).on('click', '#pagination [data-link]', (event) => {
+			const $element = $(event.currentTarget);
+			const page = +$element.data('link');
+			this.data.pagination.page = page;
+			this.render();
+			return false;
+		});
+		this.render();
 	}
 
 	form2server(key) {
@@ -165,20 +180,175 @@ class Filter {
 		return params.length ? `?${params.join('&')}` : '';
 	}
 
-	filtering() {
-		const data = this.getFilterData();
-		console.log('send to server', data);
+	filterData() {
+		const filterList = this.list.filter((item) => {
+			let isShow = true;
 
-		const currentSearch = this.buildSearch(data);
+			if (this.data.params.brand.length && !this.data.params.brand.includes(String(item?.brand?.id))) {
+				isShow = isShow && false;
+			}
+
+			if (this.data.params.manufacturer && String(this.data.params.manufacturer) !== String(item?.manufacturer?.id)) {
+				isShow = isShow && false;
+			}
+
+			if (this.data.params.model && String(this.data.params.model) !== String(item?.model?.id)) {
+				isShow = isShow && false;
+			}
+
+			if (this.data.params.year && String(this.data.params.year) !== String(item?.year)) {
+				isShow = isShow && false;
+			}
+
+			if (!(item?.price?.value >= this.data.params.price[0] && item?.price?.value <= this.data.params.price[1])) {
+				isShow = isShow && false;
+			}
+
+			return isShow;
+		});
+
+		filterList.sort((a, b) => {
+			let A = null;
+			let B = null;
+			let asc = true;
+
+			if (this.data.pagination.sort === 'price_asc') {
+				A = a?.price?.value;
+				B = b?.price?.value;
+				asc = true;
+			}
+
+			if (this.data.pagination.sort === 'price_desc') {
+				A = a?.price?.value;
+				B = b?.price?.value;
+				asc = false;
+			}
+
+			if (this.data.pagination.sort === 'new_asc') {
+				A = a?.year;
+				B = b?.year;
+				asc = true;
+			}
+
+			if (this.data.pagination.sort === 'new_desc') {
+				A = a?.year;
+				B = b?.year;
+				asc = false;
+			}
+
+			if (A > B) {
+				return asc ? 1 : -1;
+			} else if (A < B) {
+				return asc ? -1 : 1;
+			} else {
+				return 0;
+			}
+		});
+
+		return filterList;
+	}
+
+	setPagination() {
+		const result = [];
+		const perPage = this.data.pagination.perPage;
+		const page = this.data.pagination.page;
+		const length = this.filterList.length;
+
+		const count = Math.ceil(length / perPage);
+
+		let start = page - 2;
+		let end = page + 2;
+		if (start < 1) {
+			start = 1;
+		}
+		if (end > count) {
+			end = count;
+		}
+
+		if (count > 5 && start > 1) {
+			result.push({
+				href: '#',
+				text: '<<',
+				page: 1
+			});
+			result.push({
+				href: '#',
+				text: '<',
+				page: page - 1 || 1
+			});
+		}
+		for (let i = start; i <= end; i++) {
+			result.push({
+				href: i !== page ? '#' : '',
+				text: i,
+				page: i,
+				active: i === page
+			});
+		}
+		if (count > 5 && end < count) {
+			result.push({
+				href: '#',
+				text: '>',
+				page: page + 1 < count ? page + 1 : count
+			});
+			result.push({
+				href: '#',
+				text: '>>',
+				page: count
+			});
+		}
+
+		return result;
+	}
+
+	render() {
+		this.data = this.getFilterData();
+		console.log('send to server', this.data);
+
+		const currentSearch = this.buildSearch(this.data);
 		const initialSearch = this.buildSearch(this.initialData);
-		console.log(currentSearch);
-		console.log(initialSearch);
 		const search = currentSearch !== initialSearch ? currentSearch : '';
 		window.history.pushState({}, null, `${window.location.pathname}${search}`);
+
+		this.$list.html('');
+		const perPage = this.data.pagination.perPage;
+		this.filterList = this.filterData();
+		this.filterList.forEach((item, index) => {
+			if (index < perPage) {
+				const $card = this.$cardTemplate.clone();
+				$card.find('[data-brand]').html(item?.brand?.name);
+				$card
+					.find('[data-image]')
+					.attr('src', item?.image?.sizes['card-preview'])
+					.attr('alt', item?.image?.alt);
+				$card.find('[data-manufacturer]').html(item?.manufacturer?.name);
+				$card.find('[data-year]').html(item?.year);
+				$card.find('[data-model]').html(item?.model?.name);
+				$card.find('[data-currency]').html(item?.price?.currency?.symbol);
+				$card.find('[data-price]').html(item?.price?.value);
+
+				this.$list.append($card);
+			}
+		});
+
+		this.$pagination.html('');
+		this.setPagination().forEach((item) => {
+			const $paginationItem = this.$paginationTemplate.clone();
+			const $item = $paginationItem.find('[data-link]');
+			$item.html(item.text);
+			$item.attr('link', item.page);
+			if (item.href) {
+				$item.attr('href', item.href);
+			}
+			if (item.active) {
+				$item.addClass('is-active');
+			}
+			this.$pagination.append($paginationItem);
+		});
 	}
 }
 
 export default () => {
-	const filter = new Filter('#filter', '#pagination');
+	const filter = new Filter();
 	filter.$filter.data('instance', filter);
 };
